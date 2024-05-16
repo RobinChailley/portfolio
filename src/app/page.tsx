d1c8a4277af9d031b8b2c68e71e8221a6905ffc5
Robin Chailley <robin.chailley@gmail.com>
2024-05-16 08:25:08 +0200

    feat: recommendations + footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import Me from '@/sections/me/Me';
 import Contact from '@/sections/contact/Contact';
 import Projects from '@/sections/projects/Projects';
 import DownloadCVButton from '@/components/ui/DownloadCVButton';
+import Recommendations from '@/sections/recommendations/Recommendations';
+import { Github, Linkedin } from 'lucide-react';
 
 export default function Home() {
 	return (
@@ -18,6 +20,9 @@ export default function Home() {
 			<div id="experiences" className="pt-[15vh]">
 				<Experiences />
 			</div>
+			<div id="recommendations" className="pt-[15vh]">
+				<Recommendations />
+			</div>
 			<div id="about-me" className="pt-[15vh] mt-24">
 				<Me />
 			</div>
@@ -27,6 +32,20 @@ export default function Home() {
 			<div id="contact" className="pt-[15vh] w-full h-[100vh]">
 				<Contact />
 			</div>
+			<footer className="border-[1px] items-center w-full gap-12 flex px-32 h-32">
+				<div className="flex flex-col h-full justify-center gap-3">
+					<p className="text-gray-400">©️2024 - Robin Chailley</p>
+					<p className="text-gray-400">Fullstack Software Engineer</p>
+				</div>
+				<div className="flex flex-col h-full justify-center gap-3">
+					<a className="text-gray-400" target="_blank" href="https://www.linkedin.com/in/robin-chailley/">
+						<Linkedin />
+					</a>
+					<a className="text-gray-400" target="_blank" href="https://github.com/RobinChailley">
+						<Github />
+					</a>
+				</div>
+			</footer>
 		</main>
 	);
 }
---
+++ src/sections/recommendations/Recommendations.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import InfiniteMovingCards from '@/components/aceternity/MovingCards';
+
+const testimonials = [
+	{
+		quote:
+			"J'ai travaillé au sein de la même équipe que Robin sur plusieurs projets de développement de taille. Tant sur son expertise du développement fullstack que sur ses qualités professionnelles il est un développeur de grande qualité. Il est un développeur sérieux, très performant et c'est un plaisir de collaborer avec lui !",
+		quoteEn:
+			"I've worked in the same team as Robin on several major development projects. Both in terms of his expertise in fullstack development and his professional qualities, he is a top-quality developer. He's a serious, high-performance developer, and a pleasure to work with!",
+		name: 'Robin Leclerc',
+		title: 'Senior Security Engineer',
+	},
+	{
+		quote: 'Bonne communication. Sérieux et efficace ! Je recommande Robin pour son professionnalisme',
+		quoteEn: 'Good communication. Serious and efficient! I recommend Robin for his professionalism',
+		name: 'Nad Risen',
+		title: '',
+	},
+	{
+		quote:
+			"Nous avons recruté Robin CHAILLEY en tant que stagiaire, afin de sécuriser par l'intermédiaire de jeux de test unitaire et de non régression, une application permettant de gérer les organisations commerciales au sein d'un CRM (Customer Relationship Management). Robin CHAILLEY a fait preuve d'une très bonne adaptation, d'une bonne autonomie et d'une bonne expertise dans son travail qui lui ont permis par la suite d'atteindre les objectifs fixés et même les dépasser.",
+		quoteEn:
+			'We recruited Robin CHAILLEY as an intern, to use unit tests and non-regression tests to secure an application for managing sales organizations within a CRM (Customer Relationship Management) system. Robin CHAILLEY showed great adaptability, autonomy and expertise in his work, which enabled him to meet and even exceed the objectives set.',
+		name: 'Alexandre F',
+		title: 'Senior Software Engineer - La Poste',
+	},
+	{
+		quote:
+			"Robin à réalisé des pages internet, toujours un travail de qualité, ce qui en fait sa force. Afin d'augmenter les performances et la sécurité de notre plateforme web, nous avons contacté Robin pour réaliser cette mission. Réactif, impliqué et rigoureux, rien à redire sur le travail effectué.",
+		quoteEn:
+			"Robin's strength lies in the quality of his work. To increase the performance and security of our web platform, we contacted Robin to carry out this mission. Responsive, involved and rigorous, there's nothing to fault in the work he's done.",
+		name: 'Antoine',
+		title: 'CEO QuickCells',
+	},
+	{
+		quote:
+			"Je recommande chaudement les compétences de Robin, tant professionnelles que humaines ! Robin a parfaitement su s'intégrer à l'équipe et a rendu un travail de grande qualité. Excellent !",
+		quoteEn:
+			"I highly recommend Robin's professional and human skills! Robin integrated perfectly into the team and delivered high-quality work. Excellent!",
+		name: 'Hugo',
+		title: 'CEO Origame',
+	},
+	{
+		quote:
+			'Super travail avec Robin qui a su comprendre mes besoins et livrer un rendu qualitatif. Petite mention spéciale au document livré avec le site qui permet une utilisation simplifié. Merci',
+		quoteEn:
+			'Great work with Robin, who understood my needs and delivered a quality product. Special mention to the document delivered with the site, which makes it easier to use. Thank you',
+		name: 'Damien MP',
+		title: 'CEO ExpertClub',
+	},
+];
+
+export default function Recommendations() {
+	return (
+		<div className="px-12 md:px-0">
+			<h2 className="font-bold text-3xl text-center mb-12">
+				What they thought{' '}
+				<span className="relative">
+					<div className="absolute left-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[2px] w-full" />
+					about me
+				</span>
+			</h2>
+			<div className="rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+				<InfiniteMovingCards items={testimonials} direction="right" speed="slow" />
+			</div>
+		</div>
+	);
+}
