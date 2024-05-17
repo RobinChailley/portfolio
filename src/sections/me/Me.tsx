891d9c4ca02618cf679cde8abf7c48703000d2c5
Robin Chailley <robin.chailley@gmail.com>
2024-05-17 08:15:33 +0200

    feat: last fixes

--- src/sections/me/Me.tsx
+++ src/sections/me/Me.tsx
@@ -5,12 +5,12 @@ export const projects = [
 	{
 		title: '+5 years of experience',
 		description:
-			'Passionate about programming since high school, I bring over 10 years of coding experience, including more than 5 years of cumulative professional experience.',
+			'Passionate about programming since high school, I started to code 7 years ago, including more than 4 years of cumulative professional experience.',
 	},
 	{
 		title: 'Fullstack Development',
 		description:
-			'With over 7 years of experience in both frontend and backend development, I excel at creating efficient applications. My expertise in JavaScript enables me to build robust, scalable, and maintainable solutions.',
+			'With over 5 years of experience in both frontend and backend development, I excel at creating efficient applications. My expertise in JavaScript enables me to build robust, scalable, and maintainable solutions.',
 	},
 	{
 		title: 'Web Development',
@@ -46,7 +46,7 @@ export default function Me() {
 			</h2>
 			<p className="text-center dark:text-gray-400 px-12 max-w-full sm:px-0 sm:max-w-[60vw]">
 				Passionate about programming since the end of secondary school, I&apos;ve been coding in my spare and
-				professional time for over 10 years, mainly in Javascript. Closely linked to entrepreneurship, I love creating,
+				professional time for over 7 years, mainly in Javascript. Closely linked to entrepreneurship, I love creating,
 				inventing and designing.
 			</p>
 			<div className="max-w-5xl mt-12 mx-auto px-8">
